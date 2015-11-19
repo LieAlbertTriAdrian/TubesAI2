72d8beb2ae77c025ddd58574c2b214ffd011dcd7
alberttriadrian <alberttri23@gmail.com>
2015-11-18 23:47:51 -0500

    UPdate Naive Bayes

--- Naive Bayes/src/org/tusiri/ai2/NaiveBayes.java
+++ Naive Bayes/src/org/tusiri/ai2/NaiveBayes.java
@@ -1,6 +1,8 @@
 package org.tusiri.ai2;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class NaiveBayes {
 	
@@ -10,9 +12,19 @@ public class NaiveBayes {
 		public int count;
 	}
 	
+	public static class AtrKelasCountProb{
+		public String atribut;
+		public String kelas;
+		public int count;
+		public double probability;
+	}
+	
+		
+	
+	List<String> attribute = new ArrayList<String>(Arrays.asList("buying", "maint","doors","persons","lug_boot","safety"));
 	private ArrayList<Car> data;
 	private ArrayList<String> listKelas;
-	
+		
 	public NaiveBayes(ArrayList<Car> data){
 		/*ArrayList<Car> listCar = new ArrayList<Car>(data.size());
 		for (Car car : data){
@@ -44,6 +56,17 @@ public class NaiveBayes {
 			}
 		return found;
 	}
+
+	public boolean isValueFoundProb(ArrayList<AtrKelasCountProb> l, String s){
+		boolean found = false;
+			for(int i=0;i<l.size();i++){
+				if(l.get(i).atribut.equals(s)){
+					found = true;
+					break;
+				}
+			}
+		return found;
+	}	
 	
 	public void addCount(ArrayList<AtrKelasCount> l,String atribut,String kelas){
 		//Asumsi, elemen l dengan atribut = atribut dan kelas = kelas pasti ada
@@ -57,10 +80,24 @@ public class NaiveBayes {
 			i++;
 		}
 	}
+
+	public void addCountProb(ArrayList<AtrKelasCountProb> l,String atribut,String kelas){
+		//Asumsi, elemen l dengan atribut = atribut dan kelas = kelas pasti ada
+		int i=0;
+		boolean notFound = true;
+		while(i<l.size() && notFound){
+			if(l.get(i).atribut.equals(atribut) && l.get(i).kelas.equals(kelas)){
+				++l.get(i).count;
+				notFound = false;
+			}
+			i++;
+		}
+	}	
 	
 	public void countAppearance(){
 		int nAtribut = Main.NATRIBUT;
-		for(int i=0;i<nAtribut;i++){
+		int i;
+		for(i=0;i<nAtribut;i++){
 			int x=0;
 			ArrayList<AtrKelasCount> l = new ArrayList<AtrKelasCount>();
 			for (int j=0;j<data.size();j++){
@@ -79,6 +116,8 @@ public class NaiveBayes {
 				addCount(l,data.get(j).getAtr(i),data.get(j).getKelas());
 			}
 			int total=0;
+			
+			System.out.println("\n" + attribute.get(i) + " Class" + " Count");
 			for(int m=0;m<l.size();m++){
 				System.out.print(l.get(m).atribut+" ");
 				System.out.print(l.get(m).kelas+" ");
@@ -87,11 +126,64 @@ public class NaiveBayes {
 			}
 			System.out.println(total);
 		}
+	
 	}
+
+	public void constructProbabilityMatrix(){
+		int nAtribut = Main.NATRIBUT;
+		int i;
+		for(i=0;i<nAtribut;i++){
+			int x=0;
+			ArrayList<AtrKelasCountProb> l = new ArrayList<AtrKelasCountProb>();
+			for (int j=0;j<data.size();j++){
+				//Jika value atribut belum pernah ditemukan sebelumnya,
+				//Tambahkan ke l, Object AtrKelasCount sebanyak jumlah kelas, dengan nilai count 0
+				if(!(isValueFoundProb(l,data.get(j).getAtr(i)))){
+					for (int k=0;k<listKelas.size();k++){
+						AtrKelasCountProb akc = new AtrKelasCountProb();
+						akc.atribut = data.get(j).getAtr(i);
+						akc.kelas = listKelas.get(k);
+						akc.count = 0;
+						akc.probability = 0.0;
+						l.add(akc);
+					}
+				}
+				//Tambahkan data saat ini
+				addCountProb(l,data.get(j).getAtr(i),data.get(j).getKelas());
+			}
+			int total=0;
+			
+			System.out.println("\n" + attribute.get(i) + " Class" + " Count");
+			for(int m=0;m<l.size();m++){
+				System.out.print(l.get(m).atribut+" ");
+				System.out.print(l.get(m).kelas+" ");
+				System.out.print(l.get(m).count+" ");
+				double penyebut = 0;
+				for (int z =0;z< l.size();z++){
+					if (l.get(z).kelas.equals(l.get(m).kelas)){
+						penyebut += l.get(z).count;
+					}
+				}
+				if (penyebut != 0.0){
+					l.get(m).probability = l.get(m).count / penyebut;
+					System.out.println(l.get(m).probability);	
+				}
+				else{
+					System.out.println(penyebut);
+				}
+				total += l.get(m).count;
+			}
+			System.out.println(total);
+		}
+	}
+		
+		
+	
 	
 	public void process(){
 		ArrayList<String> listKelas = getListKelas();
 		System.out.println(listKelas);
-		countAppearance();
+		//countAppearance();
+		constructProbabilityMatrix();
 	}
 }
