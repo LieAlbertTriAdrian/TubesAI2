26e2b90c1021b9e776d40442d4495a77d00b840b
MarcoOrlando <13513038@std.stei.itb.ac.id>
2015-11-19 15:45:26 +0700

    Tri lencong bakar
    dibakar pakai madu

--- TubesAI/src/org/tusiri/ai2/kNN.java
+++ TubesAI/src/org/tusiri/ai2/kNN.java
@@ -7,6 +7,7 @@ public class kNN {
 	private int k;
 	private ArrayList<Instance> InstanceList;
 	
+	
 	public kNN(){
 		InstanceList = new ArrayList<Instance>();
 	}
@@ -25,6 +26,7 @@ public class kNN {
 	
 	public int HitungJarak(Instance A, Instance B){
 		int numAttributes = A.getListAtr().size();
+	
 		int jarak = 0;
 		for(int i = 0; i<numAttributes; i++){
 			if(!A.getAtr(i).equals(B.getAtr(i))){
@@ -34,12 +36,31 @@ public class kNN {
 		return jarak;
 	}
 	
-	public ArrayList<Integer> HitungJarakFull(Instance A){
-		return null;
+	public ArrayList<distance> HitungJarakFull(Instance A){
+		//Inisialisasi Array Jarak
+		ArrayList<distance> arrayJarak = new ArrayList<distance>();
+		
+		//Hitung Jarak dari Instance A ke Instance lain
+		for (Instance e : getInstanceList()){
+			if (A.getId() != e.getId()){
+				int ID = e.getId();
+				int Jarak = HitungJarak(A, e);
+				String Label = e.getKelas();
+				distance dst = new distance(ID,Jarak,Label);
+				arrayJarak.add(dst);
+			}
+		}	
+		return arrayJarak;	
 	}
 	
+	
+	
 	public void FullSet(){
-		
+		ArrayList<distance> arrayJarak = new ArrayList<distance>();
+		arrayJarak = HitungJarakFull(InstanceList.get(0));
+		for (distance e: arrayJarak){
+			System.out.println(e.getID() + " Jarak :"+e.getJarak());
+		}
 	}
 	
 	public void nFold(int fold){
